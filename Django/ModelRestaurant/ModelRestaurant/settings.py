54f6c5faebd37f2aa10485b7a0ad94fcad5bd3f4
Growing-Jiwoo <fbaa4412@naver.com>
2022-12-16 19:23:13 +0900

    Delete Secret Key

--- Django/ModelRestaurant/ModelRestaurant/settings.py
+++ Django/ModelRestaurant/ModelRestaurant/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z=cfc$$=^qm958bj2bw)zzvc2(c)9a6b1qbqenoua0m$%g(ng2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
