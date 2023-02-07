a899e9e6f77fcb8d4f31fc117d3d3bbb62bb0318
Growing-Jiwoo <fbaa4412@naver.com>
2023-02-07 17:43:57 +0900

    음식점 리스트 필터링 기능 구현중

--- src/Components/Restaurant_List.tsx
+++ src/Components/Restaurant_List.tsx
@@ -1,13 +1,44 @@
+import { useEffect } from 'react';
 import Select from 'react-select';
 import useNearRestaurangList from '../utils/useNearRestaurangList';
 
+interface RestaurantType {
+  addrjibun: string;
+  addrroad: string;
+  bsnscond: string;
+  bsnsnm: never;
+  gugun: string;
+  id: number;
+  lat: string | number;
+  lon: string | number;
+  menu: string;
+  tel: string;
+}
+
+interface MenuListType {
+  value: string | any;
+  label: string | any;
+}
+
 function RestaurantList() {
+  const menuSetList = new Set();
   const getNearRestaurangList: any = useNearRestaurangList(); // 음식점 전체 리스트
-  const options = [
-    { value: 'ar', label: 'arabic' },
-    { value: 'en', label: 'english' },
-    { value: 'fr', label: 'french' },
-  ];
+  const menuArray: { value: string; label: string }[] = [];
+
+  useEffect(() => {
+    if (getNearRestaurangList) {
+      getNearRestaurangList.map((value: RestaurantType) => {
+        menuSetList.add(value.bsnscond);
+      });
+    }
+    if (menuSetList.size !== 0) {
+      const menuList = Array.from(menuSetList);
+      menuList.map((value: any) => {
+        console.log(typeof value);
+        menuArray.push({ value: value, label: value });
+      });
+    }
+  }, [getNearRestaurangList]);
 
   return (
     <>
@@ -19,8 +50,8 @@ function RestaurantList() {
         isClearable={true}
         isRtl={true}
         isSearchable={true}
-        defaultValue={options[0]} // default option
-        options={options} // options
+        defaultValue={menuArray[0]} // default option
+        options={menuArray} // options
       />
     </>
   );
