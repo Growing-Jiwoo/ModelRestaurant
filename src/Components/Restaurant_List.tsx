d825b136ccbaf188166b615dde37a12dd28889be
Growing-Jiwoo <fbaa4412@naver.com>
2023-02-08 16:31:13 +0900

    음식점 리스트 필터링 기능 구현중

--- src/Components/Restaurant_List.tsx
+++ src/Components/Restaurant_List.tsx
@@ -16,43 +16,61 @@ interface RestaurantType {
 }
 
 interface MenuListType {
-  value: string | any;
-  label: string | any;
+  value: string;
+  label: string;
 }
 
-function RestaurantList() {
-  const menuSetList = new Set();
-  const getNearRestaurangList: any = useNearRestaurangList(); // 음식점 전체 리스트
-  const menuArray: { value: string; label: string }[] = [];
-
-  useEffect(() => {
-    if (getNearRestaurangList) {
-      getNearRestaurangList.map((value: RestaurantType) => {
-        menuSetList.add(value.bsnscond);
-      });
-    }
-    if (menuSetList.size !== 0) {
-      const menuList = Array.from(menuSetList);
-      menuList.map((value: any) => {
-        console.log(typeof value);
-        menuArray.push({ value: value, label: value });
-      });
-    }
-  }, [getNearRestaurangList]);
-
+function SelectBox(props: any): any {
   return (
     <>
       <Select
         name="name"
         className="basic-single"
-        classNamePrefix="select"
         isDisabled={false}
         isClearable={true}
         isRtl={true}
         isSearchable={true}
-        defaultValue={menuArray[0]} // default option
-        options={menuArray} // options
+        defaultValue={props.option[0]} // default option
+        options={props.option} // options
       />
+    </>
+  );
+}
+
+function CreateSelectBoxOption(list: any, array: any) {
+  list.map((value: string) => {
+    array.push({ value: value, label: value });
+  });
+}
+
+function RestaurantList() {
+  const menuSetList = new Set();
+  const locationSetList = new Set();
+
+  const getNearRestaurangList: any = useNearRestaurangList(); // 음식점 전체 리스트
+  const menuArray: { value: string; label: string }[] = [];
+  const locationArray: { value: string; label: string }[] = [];
+
+  useEffect(() => {
+    if (getNearRestaurangList) {
+      getNearRestaurangList.map((value: RestaurantType) => {
+        menuSetList.add(value.bsnscond);
+        locationSetList.add(value.gugun);
+      });
+    }
+    if (menuSetList.size !== 0) {
+      const menuList = Array.from(menuSetList);
+      const locationList = Array.from(locationSetList);
+
+      CreateSelectBoxOption(menuList, menuArray);
+      CreateSelectBoxOption(locationList, locationArray);
+    }
+  }, [getNearRestaurangList]);
+
+  return (
+    <>
+      <SelectBox option={menuArray} />
+      <SelectBox option={locationArray} />
     </>
   );
 }
