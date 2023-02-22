949d2bc6ce6789318139d799f8c25aefbb83b45e
Growing-Jiwoo <fbaa4412@naver.com>
2023-02-22 19:32:53 +0900

    Modify to ModelViewSet & Delete Secret Key

--- Django/ModelRestaurant/Restaurant/urls.py
+++ Django/ModelRestaurant/Restaurant/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import Restaurant
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('Restaurant', views.RestaurantViewSet)
 
 urlpatterns = [
-  path('Restaurant/', Restaurant, name='Restaurant'),
+    path('', include(router.urls))
 ]
--- Django/ModelRestaurant/Restaurant/views.py
+++ Django/ModelRestaurant/Restaurant/views.py
@@ -1,16 +1,8 @@
 #views.py
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import Modelrestaurant
 from .serializers import ModelrestaurantSerializer
-from rest_framework.parsers import JSONParser
-# Create your views here.
+from rest_framework import viewsets
 
-
-@csrf_exempt
-def Restaurant(request):
-    if request.method == 'GET':
-        query_set = Modelrestaurant.objects.all()
-        serializer = ModelrestaurantSerializer(query_set, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Modelrestaurant.objects.all()
+    serializer_class = ModelrestaurantSerializer
