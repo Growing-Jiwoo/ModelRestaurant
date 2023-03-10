db607ca5542ed3f760e0ed6c439c72387bfc21f0
Growing-Jiwoo <fbaa4412@naver.com>
2023-03-11 08:40:19 +0900

    최근 본 음식점 삭제 기능 구현

--- src/Components/commons/Sidebar.tsx
+++ src/Components/commons/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { RestaurantType } from '../../Type/interface';
 import { SidebarStyle } from './styled';
 
@@ -6,30 +7,45 @@ type SidebarListProps = {
 };
 
 function SidebarList(props: SidebarListProps): JSX.Element {
-  console.log(props.list);
+  const [data, setData] = useState(props.list);
+
+  const handleClick = (value: RestaurantType) => {
+    const newData = data.filter((item: RestaurantType) => item.id !== value.id);
+    setData(newData);
+    localStorage.setItem('cardData', JSON.stringify(newData));
+  };
 
   return (
     <div className="sideBar">
       <div>최근 본 음식점</div>
-      {props.list.map((item: RestaurantType) => (
+      <hr></hr>
+      {data.map((item: RestaurantType) => (
         <div key={item.id}>
-          <h2>{item.bsnsnm}</h2>
-          <p>{item.addrroad}</p>
+          <span>
+            <strong>{item.bsnsnm}</strong>
+            <button onClick={() => handleClick(item)}>X</button>
+            <br />
+          </span>
+          <span>{item.addrroad}</span>
         </div>
       ))}
     </div>
   );
 }
+// 5개 이상 넘어가면 제일 최근에 넣은거 지우기 기능 ㄱ
 
 function Sidebar(): JSX.Element {
   const cardData = JSON.parse(localStorage.getItem('cardData') || 'null');
-  if (cardData.length !== 0) {
-    console.log(cardData);
-  }
-
   return (
     <SidebarStyle>
-      {cardData.length !== 0 ? <SidebarList list={cardData} /> : <div>bye</div>}
+      {cardData ? (
+        <SidebarList list={cardData} />
+      ) : (
+        <div className="sideBar">
+          <div>최근 본 음식점</div>
+          <hr></hr>
+        </div>
+      )}
     </SidebarStyle>
   );
 }
