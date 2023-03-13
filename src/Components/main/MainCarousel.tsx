55e98d86c11dd8ec2d547c98f535a0a66038eb64
Growing-Jiwoo <fbaa4412@naver.com>
2023-03-13 14:52:00 +0900

    랜덤 음식점 추천 배너 기능 구현

--- src/Components/main/MainCarousel.tsx
+++ src/Components/main/MainCarousel.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { BannerLogoStyle } from './styled';
+import { useNavigate } from 'react-router-dom';
+import useNearRestaurangList from '../../Hooks/useNearRestaurangList';
 
 function ControlledCarousel() {
   const [index, setIndex] = useState<number>(0);
+  const getNearRestaurangList = useNearRestaurangList(null);
+  const [count, setCount] = useState<number>(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getNearRestaurangList.length !== 0) {
+      console.log(getNearRestaurangList);
+      setCount(getNearRestaurangList.length);
+    }
+  }, [getNearRestaurangList]);
 
   const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
   };
+
+  function handleOnClick() {
+    if (count !== 0) {
+      const randomNum = Math.floor(Math.random() * count) + 1;
+      navigate(`/list/${randomNum}`);
+    }
+  }
 
   return (
     <div>
@@ -18,6 +37,9 @@ function ControlledCarousel() {
               className="random_banner"
               alt="bannerLogo"
               src={`${process.env.PUBLIC_URL}/img/randomBanner.PNG`}
+              onClick={() => {
+                handleOnClick();
+              }}
             />
           </Carousel.Item>
           <Carousel.Item>
