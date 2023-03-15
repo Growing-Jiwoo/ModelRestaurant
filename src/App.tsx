88a7168bef118f9a4a1e6bdb496bf5e571e91c11
Growing-Jiwoo <fbaa4412@naver.com>
2023-03-15 21:03:38 +0900

    메인 배너 컴포넌트 분리 및 로그인 UI path 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import { useEffect } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import {
-  Routes,
-  Route,
-  useNavigate,
-  Outlet,
-  useLocation,
-} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import MainComponent from './Components/main/HomeUi';
+import LoginUi from './Components/login/LoginUi';
 import Map from './Components/map/Map';
 import RestaurantList from './Components/list/RestaurantList';
 import NavBar from './Components/commons/Navbar';
 import Footer from './Components/commons/Footer';
-import Sidebar from './Components/commons/Sidebar';
 import theme from './Style/theme';
 import DetailRestaurantInfo from './Components/list/DetailRestaurantInfo';
-
-const MainBanner = styled.div`
-  ${({ theme }) => theme.common.flexCenterColumn};
-  ${({ theme }) => theme.animation.slow_fadein_fadeout};
-  margin-top: 23vh;
-  min-height: 70vh;
-`;
-
-function MainBannerImg() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('/home');
-    }, 2000);
-  }, []);
-  return (
-    <div>
-      <MainBanner>
-        <img
-          className="main_banner_image"
-          alt="bannerLogo"
-          src={process.env.PUBLIC_URL + '/img/bannerLogo.PNG'}
-        />
-      </MainBanner>
-    </div>
-  );
-}
 
 function App() {
   return (
@@ -52,13 +17,13 @@ function App() {
       >
         <div style={{ flex: 1 }}>
           <Routes>
+            <Route path="/" element={<LoginUi />} />
             <Route element={<MainLayout />}>
               <Route path="/home" element={<MainComponent />} />
               <Route path="/map" element={<Map />} />
               <Route path="/list" element={<RestaurantList />} />
               <Route path="/list/:id" element={<DetailRestaurantInfo />} />
             </Route>
-            <Route path="/" element={<MainBannerImg />} />
           </Routes>
         </div>
       </div>
@@ -67,14 +32,9 @@ function App() {
 }
 
 const MainLayout = () => {
-  const location = useLocation();
-
-  const showSidebar = location.pathname === '/home';
-
   return (
     <>
       <NavBar />
-      {showSidebar && <Sidebar />}
       <Outlet />
       <Footer />
     </>
