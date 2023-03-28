2bb064ab3b07dffa4b0f0c8805366e2b581b8d6c
Growing-Jiwoo <fbaa4412@naver.com>
2023-03-28 15:47:45 +0900

    음식점 별 클릭한 횟수 저장 기능 구현

--- src/Utils/recentRestaurant.tsx
+++ src/Utils/recentRestaurant.tsx
@@ -4,21 +4,39 @@ import type { RestaurantType } from '../Type/interface';
 export function recentRestaurant() {
   const navigate = useNavigate();
 
-  function handleClick(value: RestaurantType) {
+  function handleClick(value: RestaurantType): void {
     const previousData = localStorage.getItem('cardData');
-    const dataArray = previousData ? JSON.parse(previousData) : [];
+    const viewCntData = localStorage.getItem('viewCnt');
 
-    const dataExists = dataArray.some(
+    const previousDataArray: RestaurantType[] = previousData
+      ? JSON.parse(previousData)
+      : [];
+    const viewCntDataArray: { id: string; viewCnt: number }[] = viewCntData
+      ? JSON.parse(viewCntData)
+      : [];
+
+    const previousDataIndex: number = previousDataArray.findIndex(
       (item: RestaurantType) => item.id === value.id
     );
-    if (!dataExists) {
-      if (dataArray.length === 3) {
-        dataArray.shift();
+
+    if (previousDataIndex === -1) {
+      if (previousDataArray.length === 3) {
+        previousDataArray.shift();
       }
-      const newData = [...dataArray, value];
-      localStorage.setItem('cardData', JSON.stringify(newData));
+      previousDataArray.push(value);
+      localStorage.setItem('cardData', JSON.stringify(previousDataArray));
+    }
+    const viewCntDataIndex: number = viewCntDataArray.findIndex(
+      (item: { id: string; viewCnt: number }) => item.id === String(value.id)
+    );
+
+    if (viewCntDataIndex === -1) {
+      viewCntDataArray.push({ id: String(value.id), viewCnt: 1 });
+    } else {
+      viewCntDataArray[viewCntDataIndex].viewCnt++;
     }
 
+    localStorage.setItem('viewCnt', JSON.stringify(viewCntDataArray));
     navigate(`/list/${value.id}`);
   }
 
