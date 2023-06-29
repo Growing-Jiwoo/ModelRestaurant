946ea446421b99fe3de4f7f5fc7af826a84b512a
Growing-Jiwoo <fbaa4412@naver.com>
2023-06-29 10:18:36 +0900

    디렉토리 구조 변경 및 파일 이동에 따른 코드 변화

--- Client/src/hook/useAxiosWithAuth.ts
+++ Client/src/hook/useAxiosWithAuth.ts
@@ -12,9 +12,6 @@ const useAxiosWithAuth = (): AxiosInstance => {
 
   const axiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/',
-    headers: {
-      Authorization: `Bearer ${cookies.jwt}`,
-    },
   });
 
   axiosInstance.interceptors.request.use(
