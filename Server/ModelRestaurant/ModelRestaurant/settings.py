cc9633eaafa366a04e996bd1efa0f2d3c1065b7b
Growing-Jiwoo <fbaa4412@naver.com>
2023-06-29 10:27:07 +0900

    Secret Key 동적 생성으로 변경

--- Server/ModelRestaurant/ModelRestaurant/settings.py
+++ Server/ModelRestaurant/ModelRestaurant/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key
+
+# Generate a new secret key
+secret_key = get_random_secret_key()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'NEbRLqfe071M9-uILB9WW4nkf6a0ksQ8e-YH3hEqJQ0'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
